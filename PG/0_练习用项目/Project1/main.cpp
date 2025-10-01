218d26e66ce2bb36daa2d6532af1747d94fb4318
2904775368 <wch0126whj@gmail.com>
2025-10-01 11:42:33 +0900

    更新

--- PG/0_练习用项目/Project1/main.cpp
+++ PG/0_练习用项目/Project1/main.cpp
@@ -2,7 +2,7 @@
 #include "calc.h"
 
 int main() {
-	int a = 5, b = 10;
+	int a = 6, b = 10;
 	float x = 5.5f, y = 10.5f;
 	printf("Sum of %d and %d is %d\n", a, b, Power(a, b));
 	printf("Sum of %.2f and %.2f is %.2f\n", x, y, Power(x, y));
